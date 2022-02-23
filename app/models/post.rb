ba8d34b2b9a85c8a87bd575f732687d3bdddbc3c
Justin Luciano <justinlluciano@gmail.com>
2022-02-23 15:26:08 -0800

    commit tree ca68f4d471d6e80c97f3675bb97ecd96e73a2d29

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait_title
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def click_bait_title
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "Sorry, not click-baity enough")
+    end
+  end
 end
